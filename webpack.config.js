6bdb7f720c73a8e17b69af6ae4fcc3c3940de844
Sabari Sakthivel <xarcoffl@gmail.com>
2026-08-22 10:04:49 +0530

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://YOUR_GITHUB_USERNAME.github.io/YOUR_REPO_NAME/"; // TODO: replace with your actual GitHub Pages URL (must end with /)
+const urlProd = "https://Xarcoffl.github.io/Certificate-Generation-System/"; // TODO: replace with your actual GitHub Pages URL (must end with /)
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
